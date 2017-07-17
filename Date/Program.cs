a53c7bb0da5fcda242375e2df7956985748ebb98
Carl Matthew Hamilton <info@chtsi.uk>
2017-07-17 22:48:28 +0100

    Test
    test

--- Date/Program.cs
+++ Date/Program.cs
@@ -10,6 +10,32 @@ namespace Date
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Your Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if(hoursOfSleep >= 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+                Console.WriteLine("NOOOOO! We have to work tomorrow");
+            else if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                Console.WriteLine("Another hard slog to come tomorrow!");
+         
+                Console.ReadKey();
+
+
+
+            }
         }
     }
 }
